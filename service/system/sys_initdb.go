371997841786651f968f5639190e822cea9de5f3
fisherkaiyu <fisherkaiyu@tencent.com>
2023-07-13 20:28:07 +0800

    init db

---
+++ service/system/sys_initdb.go
@@ -0,0 +1,9 @@
+package system
+
+const (
+	Mysql           = "mysql"
+	InitSuccess     = "[%v]-->初始化✅\n"
+	InitDataExist   = "[%v]-->初始化数据已存在\n"
+	InitDataFailed  = "[%v]-->初始化数据失败❌️！\n"
+	InitDataSuccess = "[%v]--> 初始化数据✅\n"
+)
